489ea1857d462c1a889a4c6d0835ab8c6e5c562b
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-11-06 19:12:52 +0900

    ポイント機能の非同期

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,8 +1,20 @@
 class PointsController < ApplicationController
   def create
-    # @point = Point.new(point_params)
-    Point.create!(point: 1, user_id: current_user.id)
-    redirect_to :root
+    @point = Point.new(point_params)
+    if @point.save
+      @points = Point.where(user_id: current_user.id).count
+      # binding.pry
+      respond_to do |format|
+        format.html #{redirect_to omikuji_path}
+        format.json { render json: @points}
+      end
+    else
+      redirect_to :root
+    end
   end
 
+  private
+  def point_params
+    params.require(:point).permit(:point).merge(user_id: current_user.id)
+  end
 end
