bc0e9bd8637dbc72bf0b70cb5507232c1d712333
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-04 20:15:11 +0900

    rooting追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "games#index"
-  resources :games do
+  resources :games ,only: :index do
     collection do
       get "quize"
+      get "timer"
       get "omikuji"
       get "slot"
       get "touch"
     end
   end
-  
+
   resources :points, only: :create
 end
