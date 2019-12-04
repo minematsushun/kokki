151ca5c8e079d0429580f1040d8447eedfc5ef89
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-04 20:01:59 +0900

    resouceに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "games#index"
+  resources :games do
+
   get "games" => "games#index"
   get "quize" => "games#quize"
   get "timer" => "games#timer"
